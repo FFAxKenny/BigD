056414238aef762d695bca8f62987206f80279e8
ejay <ejay_hi@icloud.com>
2013-04-21 13:58:56 -1000

    UnitTest.c now include stack tests

--- high/test/UnitTests.c
+++ high/test/UnitTests.c
@@ -1,9 +1,9 @@
-#include"../../util.h"
+#include"../util.h"
 
 #include<CUnit/CUnit.h>
 #include<CUnit/Basic.h>
 
-#include "../../src/env/maze.h"
+#include "../src/mouse/mouse.h"
 
 int init_suite(void) { return 0; }
 int clean_suite(void) { return 0; }
@@ -48,59 +48,75 @@ void test_uploadMaze(void) {
 }
 
 void test_Stack(void) {
-	Stack * stack;
+	Stack * stack = (Stack*) malloc(sizeof(Stack));
 	Coordinates * this=(Coordinates*)malloc(sizeof(Coordinates));
 	Coordinates * that=(Coordinates*)malloc(sizeof(Coordinates));
 	this->row = 5;
 	this->col = 10;
 	that->row = 10;
 	that->col = 5;
-	push(&stack,this);
-	push(&stack,that);
-	Coordinates * result;
-	result = pop(&stack);
+	push(stack,this);
+	push(stack,that);
+	Coordinates * result = (Coordinates*) malloc(sizeof(Coordinates));
+	result = pop(stack);
 	CU_ASSERT_EQUAL(result->row,10);
 	CU_ASSERT_EQUAL(result->col,5);
-	result = pop(&stack);
+	result = pop(stack);
 	CU_ASSERT_EQUAL(result->row,5);
 	CU_ASSERT_EQUAL(result->col,10);
+	free(stack);
+	free(this);
+	free(that);
+	free(result);
 }
 
 int main( void ) {
-    CU_pSuite pSuite = NULL;
+    CU_pSuite mazeSuite = NULL;
+    CU_pSuite stackSuite = NULL;
 
     // initialize the CUnit test registry
     if( CUE_SUCCESS != CU_initialize_registry() ) return CU_get_error();
 
     // add a suite to the registry
-    pSuite = CU_add_suite("maze_test_suite", init_suite, clean_suite);
-    if( pSuite == NULL ) {
+    mazeSuite = CU_add_suite("maze_test_suite", init_suite, clean_suite);
+    if( mazeSuite == NULL ) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
     // add the tests to the suite
-    if( CU_add_test(pSuite, "test_uploadMaze", test_uploadMaze)==NULL) {
+    if( CU_add_test(mazeSuite, "test_uploadMaze", test_uploadMaze)==NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+    
+    stackSuite = CU_add_suite("stack_test_suite", init_suite, clean_suite);
+    if( mazeSuite == NULL ) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
+    if( CU_add_test(stackSuite, "test_Stack", test_Stack)==NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+/*
     // Run all tests using the basic interface
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     printf("\n");
     CU_basic_show_failures(CU_get_failure_list());
     printf("\n\n");
-
+*/
 /*    
       //Run all tests using the automated interface
       CU_automated_run_tests();
       CU_list_tests_to_file();
       */
-/*
+
       //Run all tests using the console interface
       CU_console_run_tests();
- */ 
+ 
     // Clean up registry and return
     CU_cleanup_registry();
     return CU_get_error();
